c0deda031188651eb737cc9660e3226f5e05c3e2
david <daviddecastro42@live.co.uk>
2020-04-29 01:50:38 +0100

    adding a Userslist page component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UserList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'David Decastro',
+      image:
+        'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg',
+      place: 3
+    }
+  ];
+  return <UserList item={USERS} />;
 };
 
 export default Users;
