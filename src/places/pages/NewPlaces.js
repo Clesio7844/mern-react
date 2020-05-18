4415a4233b1b274b9ca67dd431ea4a0510858564
david <daviddecastro42@live.co.uk>
2020-05-19 00:58:09 +0100

    adding managing state in the input component and adding input validation

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/input';
+
+import './NewPlace.css';
+
 const NewPlace = () => {
-  return <h2>NewPlace Works</h2>;
+  return (
+    <form className='place-form'>
+      <Input
+        element='input'
+        type='text'
+        label='Title'
+        validators={[]}
+        errorText='Please enter a valid title.'
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
